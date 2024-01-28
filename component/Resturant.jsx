31bc51d4ee9457aed57df275962818eb577b1d0c
vivekpoddar10 <vivek.kumar.poddar.01@gmail.com>
2024-01-28 14:46:31 +0530

    DEV - fetched the resturant list from, and displayed the basic info

---
+++ component/Resturant.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { IMAGE_URL } from "../utils/constant";
+
+const Resturant = (props) => {
+  const { info } = props;
+  const { name, locality, avgRating, costForTwo, cloudinaryImageId, cuisines } =
+    info;
+  return (
+    <div className="resturant">
+      <div className="food-image">
+        <img src={IMAGE_URL + cloudinaryImageId} alt="food image"></img>
+      </div>
+      <div className="resturant-name">{name}</div>
+      <div className="resturant-address">{locality}</div>
+      <div className="food-cuisine">{
+      cuisines.filter((value, index) => index < 3).join()}...</div>
+      <div className="price-rating">
+        <div className="price">{costForTwo}</div>
+        <div className="rating">{avgRating}⭐</div>
+      </div>
+    </div>
+  );
+};
+
+export default Resturant;
