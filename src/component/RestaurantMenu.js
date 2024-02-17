d49e52dc05ac0b262700720c8119ae2f978ef295
vivekpoddar10 <vivek.kumar.poddar.01@gmail.com>
2024-02-17 16:47:50 +0530

    feat - restaurant menu veg filter
    We can filter out all the veg menus of different category by checking a checkbox

--- src/component/RestaurantMenu.js
+++ src/component/RestaurantMenu.js
@@ -1,10 +1,34 @@
-import { useEffect, useState } from "react";
-import Restaurant from "./RestaurantCard";
-const RestaurantMenu = () => {
-  
+import { IMAGE_URL } from "../utils/constant";
+const RestaurantMenu = (props) => {
+  const { info } = props;
+  const {
+    category,
+    description,
+    imageId,
+    inStock,
+    name,
+    itemAttribute,
+    offerTags,
+    price,
+  } = info;
+
+
   return (
-    <div>
-      
+    <div className="carousel">
+      <div className="carousel-image-div">
+        <img src={IMAGE_URL + imageId} className="carousel-image"></img>
+      </div>
+      <div className="carousel-food-details">
+        <div className="carousel-food-name">{name}</div>
+        <div className="carousel-food-desc">{description}</div>
+        <div className="carousel-food-catg">{itemAttribute.vegClassifier}</div>
+      </div>
+
+      <div className="carousel-price-details">
+        <div className="carousel-price">₹{price / 100}</div>
+        <div className="carousel-price-offer">{offerTags ? offerTags[0].title : ''} </div>
+        <div className="carousel-price-offer-coupon">{offerTags ? offerTags[0].subTitle : ''} </div>
+      </div>
     </div>
   );
 };
