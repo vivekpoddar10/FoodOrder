d88d531fef5960ee173173f6b92c4c8b3f9d5aa8
vivekpoddar10 <vivek.kumar.poddar.01@gmail.com>
2024-03-02 15:36:34 +0530

    refactor the code due to api change

--- src/component/About.js
+++ src/component/About.js
@@ -1,22 +1,11 @@
+import React from "react";
 
-import UserClass from './UserClass';
-import React from 'react';
-
-class About extends React.Component {
-  constructor() {
-    super();
-    console.log('Parent constructor called');
-  }
-  componentDidMount() {
-    console.log('parent mount called');
-  }
-  render() {
-    console.log('parent render called');
-    return <div>
-      <UserClass name={'first'}/>
-      <UserClass name={'second'}/>
+const About = () => {
+  return (
+    <div>
+      <h1>This is About page</h1>
     </div>
-  }
-}
+  );
+};
 
 export default About;
--- src/utils/useRestaurant.js
+++ src/utils/useRestaurant.js
@@ -3,7 +3,6 @@ import { MENU_API } from "./constant";
 
 const useRestaurant = (resId) => {
   const [resInfo, setResInfo] = useState([]);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetchData();
@@ -12,15 +11,14 @@ const useRestaurant = (resId) => {
   const fetchData = async () => {
     try {
       const data = await fetch(MENU_API + resId);
-      if(!data.ok) {
-        throw new Error('No Data Found')
+      if (!data.ok) {
+        throw new Error("No Data Found");
       }
       const jsonData = await data.json();
       setResInfo([
-        jsonData.data.cards[2].card.card.info,
-        jsonData.data.cards[3].card.card.gridElements.infoWithStyle.offers,
-        jsonData.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card
-          .card.itemCards,
+        jsonData.data.cards[0].card.card.info,
+        jsonData.data.cards[1].card.card.gridElements.infoWithStyle.offers,
+        jsonData.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards,
       ]);
     } catch (error) {}
   };
